a33887acd38f288515a6b7f3a0aa9be1053c36a8
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2022-11-08 22:55:06 -0300

    forms atualizado django

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -6,3 +6,11 @@ class formularioLancar(forms.Form):
     descricao = forms.CharField(max_length=50)
     categoria = forms.CharField(max_length=50)
     valor = forms.FloatField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['data'].widget.attrs['class'] = 'form-control mb-3 date'
+        self.fields['data'].widget.attrs['placeholder'] = 'Digite uma data...'
+        self.fields['descricao'].widget.attrs['class'] = 'form-control mb-3'
+        self.fields['categoria'].widget.attrs['class'] = 'form-control mb-3'
+        self.fields['valor'].widget.attrs['class'] = 'form-control mb-3'
