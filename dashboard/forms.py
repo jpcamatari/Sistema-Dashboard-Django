ab79a7b4c522813083d1c2a4db7f9439bfa156a9
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2022-11-10 23:09:41 -0300

    Remoção msg de erro do form

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -10,6 +10,10 @@ class formularioLancar(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['data'].widget.attrs['class'] = 'form-control mb-3 date'
+        self.fields['data'].error_messages['required'] = ""
         self.fields['descricao'].widget.attrs['class'] = 'form-control mb-3'
+        self.fields['descricao'].error_messages['required'] = ""
         self.fields['categoria'].widget.attrs['class'] = 'form-control mb-3'
+        self.fields['categoria'].error_messages['required'] = ""
         self.fields['valor'].widget.attrs['class'] = 'form-control mb-3'
+        self.fields['valor'].error_messages['required'] = ""
