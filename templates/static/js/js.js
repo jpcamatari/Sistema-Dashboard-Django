69b3466cd7a0e3cb4ebbc55f6282190521fb1065
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2022-11-23 20:57:19 -0300

    Conectando front com back

--- templates/static/js/js.js
+++ templates/static/js/js.js
@@ -1,1 +1,10 @@
-console.log('teste')
+
+function renderiza_total_gasto(url){
+    fetch(url, {
+        method: 'get',
+    }).then(funcition(result){
+        return result.json()
+    }).then(funcition(data){
+        document.getElementById('total_gasto').innerHTML = data.total
+    })
+}
