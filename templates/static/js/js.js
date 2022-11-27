514f07321ac7d6b5c9b303bd5043d5c5a029048a
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2022-11-26 22:26:58 -0300

    function grafico mensal custo

--- templates/static/js/js.js
+++ templates/static/js/js.js
@@ -8,3 +8,36 @@ function renderiza_total_gasto(url){
     })
 
 }
+
+function renderiza_faturamento_mensal(url){
+
+    fetch(url, {
+        method: 'get',
+    }).then(function(result){
+        return result.json()
+    }).then(function(data){
+
+        const ctx = document.getElementById('faturamento_mensal').getContext('2d');
+        var cores_faturamento_mensal = gera_cor(qtd=12)
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+                datasets: [{
+                    label: data.labels,
+                    data: data.data,
+                    backgroundColor: cores_faturamento_mensal[0],
+                    borderColor: cores_faturamento_mensal[1],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    })
+}
