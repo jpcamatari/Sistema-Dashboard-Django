6fbeb93070ab92e9855cf52a97784756aa27ed83
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2022-11-28 22:38:54 -0300

    função de gerador de cor

--- templates/static/js/js.js
+++ templates/static/js/js.js
@@ -9,6 +9,21 @@ function renderiza_total_gasto(url){
 
 }
 
+function gera_cor(qtd=1){
+    var bg_color = []
+    var border_color = []
+    for(let i = 0; i < qtd; i++){
+        let r = Math.random() * 255;
+        let g = Math.random() * 255;
+        let b = Math.random() * 255;
+        bg_color.push(`rgba(${r}, ${g}, ${b}, ${0.2})`)
+        border_color.push(`rgba(${r}, ${g}, ${b}, ${1})`)
+    }
+    
+    return [bg_color, border_color];
+}
+
+
 function renderiza_faturamento_mensal(url){
 
     fetch(url, {
